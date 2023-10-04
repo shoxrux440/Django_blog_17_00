088be925983fcdf56d8a8d9873ab8780c7537322
student <student@intranet.proweb.uz>
2023-10-04 17:17:41 +0500

    added

---
+++ main.py
@@ -0,0 +1,7 @@
+# библиотеки (django, pillow)
+# создание проекта на django
+# django-admin - специальная команда для запуска других команд
+# django-admin startproject project_name - создание нового проекта
+# python manage.py runserver - запускает тестовый сервер
+# python manage.py migrate - отправляет файл с миграциями на выполнение в базу данных
+
